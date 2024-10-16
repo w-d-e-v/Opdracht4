0c713dd923f4be6629eab84fe1315639f3cd1670
Wessel <w.m.de.vroome@st.hanze.nl>
2024-10-16 20:37:14 +0200

    landing controller toevoegen

---
+++ src/Controller/LandingController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class LandingController extends AbstractController {
+    #[Route('/', name: 'Landing Page')]
+    public function index(): Response {
+        return $this->render('landing/index.html.twig', [
+            'controller_name' => 'LandingController',
+        ]);
+
+}

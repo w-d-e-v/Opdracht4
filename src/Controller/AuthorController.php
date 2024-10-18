cc8cde9f23549c38d4c44bcc6def1054fbd44999
Wessel <w.m.de.vroome@st.hanze.nl>
2024-10-18 20:39:06 +0200

    Author Multatuli wordt aangemaakt! Whoohooo!

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Author;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityManager;
 
 class AuthorController extends AbstractController
 {
@@ -15,4 +18,20 @@ class AuthorController extends AbstractController
             'controller_name' => 'AuthorController',
         ]);
     }
+
+    #[Route('/author/create', name: 'app_author_create')]
+    public function create(EntityManagerInterface $em): Response
+    {
+
+        $author = new Author();
+        $author->setName('Multatuli');
+
+        $em->persist($author);
+        $em->flush();
+
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+
 }

1c8905a1ac610d7e525e1060ca40bdc0be68f77b
Wessel <w.m.de.vroome@st.hanze.nl>
2024-10-19 13:14:02 +0200

    Toevoegen boek (moet nog wel een dropdown met aanwezige auteurs bij) in twig formulier + een lijst genereren van aanwezige boeken

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Book;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityManager;
 
 class BookController extends AbstractController
 {
@@ -15,4 +19,34 @@ class BookController extends AbstractController
             'controller_name' => 'BookController',
         ]);
     }
+
+    #[Route('/book/create', name: 'app_book_create')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+{
+    //haal de waarden an het webformulier uit de post
+    $title = $request->request->get('title');
+    $author = $request->request->get('author');
+    $isbn = $request->request->get('isbn');
+
+    $book = new Book();
+    $book->setTitle($title); //geen dubbele quotes gebruiken rond een variabele 🤡
+    $book->setAuthor($author);
+    $book->setIsbn($isbn);
+
+    $em->persist($book);
+    $em->flush();
+
+    return new Response("Book " . $book->getTitle() . " is aangemaakt");
+
+}
+    #[Route('/book/list', name: 'app_book_list')]
+    public function list(EntityManagerInterface $em): Response {
+        $books = $em->getRepository(Book::class)->findAll();
+        return $this->render('book/list.html.twig', [
+            'books' => $books,
+        ]);
+    }
+
+
+
 }

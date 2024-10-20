ae0c01ba0df2273a4b7e23e1ea9a0b5301ff1633
Wessel <w.m.de.vroome@st.hanze.nl>
2024-10-20 11:42:54 +0200

    Nieuwe klanten aanmaken

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,4 +18,22 @@ class CustomerController extends AbstractController
             'controller_name' => 'CustomerController',
         ]);
     }
+
+    #[Route('/customer/create', name: 'app_customer_create')]
+    public function create(Request $request, EntityManagerInterface $em): Response {
+
+        //haal de waarden an het webformulier uit de post
+        $name = $request->request->get('name');
+        $address = $request->request->get('address');
+
+        $customer = new Customer(); //maak object aan
+        $customer->setName($name);    //vul het
+        $customer->setAddress($address);  //object metd e relevante waarden
+
+        $em->persist($customer);
+        $em->flush(); //en doorspoelen maar
+
+        return new Response("Klant " . $customer->getName() . " is aangemaakt");
+    }
+
 }
